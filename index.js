809991d81d8bb70f909abd5f4c4651c0cfdfeece
edavis06 <ed210774@gmail.com>
2023-03-13 23:09:53 +0000

    Commentless getDistance function

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 (function(window, _) {
     window.sparky = window.sparky || {
         numz: {},
-        phyz: {},
+        phyz: {
+            /**
+             * 
+             * @param {*} pointA 
+             * @param {*} pointB 
+             * @returns the distance between the pointsA and pointsB. It is always positive, and directionless
+             */
+            getDistance(pointA, pointB){
+                const
+                    distanceX = pointB.x - pointA.x,
+                    distanceY = pointB.y - pointA.y,
+                    distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+                return distance;
+            }
+        },
     };
 }(window, window._));
