d8dceb01186ff2e6037ed64ccdf077ceceba9393
edavis06 <ed210774@gmail.com>
2023-03-15 00:32:48 +0000

    added comments and added radian funcitons

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 (function(window, _) {
     window.sparky = window.sparky || {
-        numz: {},
+        numz: {
+            /**
+             * 
+             * @param {number} degrees 
+             * @returns the degrees in radians
+             */
+            degreesToRadians(degrees) {
+                return degrees * Math.PI / 180;
+            },
+            /**
+             * 
+             * @param {number} degrees 
+             * @returns the degrees in radians
+             */
+            radiansToDegrees(radians){
+                return radians * 180 / Math.PI;
+            },
+            /**
+             * 
+             * @param {object} pointA 
+             * @param {number} pointA.x 
+             * @param {number} pointA.y 
+             * @param {object} pointB
+             * @param {number} pointB.x 
+             * @param {number} pointB.y  
+             * @returns The angle between the two points in degrees
+             */
+            getAngleDegrees(pointA, pointB){
+                const
+                    distanceX = pointB.x - pointA.x,
+                    distanceY = pointB.y - pointA.y,
+                    radians = Math.atan2(distanceY, distanceX),
+                    degrees = radians * 180 /Math.PI;
+                return degrees;
+            },
+
+        },
         phyz: {
             /**
              * 
-             * @param {*} pointA 
-             * @param {*} pointB 
+             * @param {object} pointA 
+             * @param {number} pointA.x 
+             * @param {number} pointA.y 
+             * @param {object} pointB
+             * @param {number} pointB.x 
+             * @param {number} pointB.y 
              * @returns the distance between the pointsA and pointsB. It is always positive, and directionless
              */
             getDistance(pointA, pointB){
